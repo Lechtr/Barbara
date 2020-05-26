215942e184536451271a29061c6d57e928b72dd5
Joshua Friedrich <joshua.friedrich@stud-mail.uni-wuerzburg.de>
2020-05-26 18:46:01 +0200

    Erster Skill - Barbara Name

--- __init__.py
+++ __init__.py
@@ -9,27 +9,37 @@ class BarbaraSkill(MycroftSkill):
         MycroftSkill.__init__(self)
 
     def initialize(self):
+        # Word entity ist Platzhalter für beliebieges Wort
         self.register_entity_file("word.entity")
-        self.parser = WiktionaryParser()
+        # self.parser = WiktionaryParser()
 
-    @intent_file_handler('fallback.wiktionary.definition.intent')
-    def handle_wiktionary_definition(self, message):
-        #Get word to define from utterance
-        word = message.data.get('word')
-        #Lookup the word using Wiktionary
-        get_word_info = self.parser.fetch(word)
 
-        #Speak definition for requested word back to user
-        try:
-            # Get first definition from wiktionary response
-            response = get_word_info[0]['definitions'][0]['text'][1]
-            # Log the definition
-            LOG.info(response)
-            self.speak_dialog('fallback.wiktionary', {'word': word, 'definition': response})
-        except:
-            self.speak_dialog('error.wiktionary')
+    @intent_file_handler('barbara.what.is.name.intent')
+    def handle_barbara_name(self, message):
+        self.speak_dialog('barbara.name.introduction')
+
+    # @intent_file_handler('fallback.wiktionary.definition.intent')
+    # def handle_wiktionary_definition(self, message):
+        # #Get word to define from utterance
+        # word = message.data.get('word')
+        # #Lookup the word using Wiktionary
+        # get_word_info = self.parser.fetch(word)
+
+        # #Speak definition for requested word back to user
+        # try:
+            # # Get first definition from wiktionary response
+            # response = get_word_info[0]['definitions'][0]['text'][1]
+            # # Log the definition
+            # LOG.info(response)
+            # self.speak_dialog('fallback.wiktionary', {'word': word, 'definition': response})
+        # except:
+            # self.speak_dialog('error.wiktionary')
+
+
+    def stop(self):
+        pass
 
 
 def create_skill():
-    return WiktionarySkill()
+    return BarbaraSkill()
 
